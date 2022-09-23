a2ed477f539b66d9498e2740ed9693a4094309b6
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-09-23 16:43:35 -0700

    tupals

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Some text goes here...");
+// const person: {
+//     name: string;
+//     age: number;
+// }
+var person = {
+    name: "Anthony",
+    age: 23,
+    hobbies: ["Sports", "Music", "Coding"]
+};
+var favActivities;
+favActivities = ["Boxing"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+;
