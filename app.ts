f32169ca7b8a44bfd505eb9d387cbaf27c342444
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-09-26 15:22:39 -0700

    Enums

--- app.ts
+++ app.ts
@@ -2,11 +2,34 @@
 //     name: string;
 //     age: number;
 // }
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name: "Anthony",
+//     age: 23,
+//     hobbies: ["Sports", "Music", "Coding"],
+//     role: [2, 'author']
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {ADMIN, READ_ONLY, AUTHOR}; // Assigns labels to numbers
 const person = {
     name: "Anthony",
     age: 23,
-    hobbies: ["Sports", "Music", "Coding"]
+    hobbies: ["Sports", "Music", "Coding"],
+    role: Role.ADMIN
 };
+if(person.role === Role.ADMIN){
+    console.log("is admin.");
+}
+// person.role.push("Admin");
+//push() is an exception to tupals ex. role
+// person.role[1] = 10;
 
 let favActivities: string[];
 favActivities = ["Boxing"];
