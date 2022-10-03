0109178a3bd25d36110a0ef255178f0c64244e2d
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-10-03 15:08:25 -0700

    Void and undefined

--- app.ts
+++ app.ts
@@ -1,23 +1,8 @@
-type Combinable = number | string;  
-type ConversionDescriptor = 'as-text' | 'as-number'; 
-function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor){
-    let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === "as-number"){
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    if(resultConversion === "as-number"){
-        return +result
-    } else {
-        return result.toString();
-    }
-    return result;
-};
+function add(n1: number, n2: number): number{
+    return n1 + n2;
+}
+function printResult(num: number){ 
+    console.log('Result: ' + num);
+}
 
-const combinedAges = combine(30, 26, "as-number");
-const combinedStringAges = combine("30", "26", "as-number");  
-console.log(combinedStringAges);
-const combinedName = combine("sarah", "tony", "as-text");
-console.log(combinedAges);
-console.log(combinedName);
+printResult(add(5, 12));
